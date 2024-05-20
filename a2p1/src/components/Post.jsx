ea53ad414bd0194311d07b46a80d4e697529e079
Swarnava-paul <swarnavapaul108@gmail.com>
2024-05-20 19:10:26 +0530

    react assaignmen 2 1

---
+++ a2p1/src/components/Post.jsx
@@ -0,0 +1,10 @@
+function Post({ name,email }) {
+  return (
+    <div>
+      <p>Name : {name.firstname} {name.lastname}</p>
+      <p>Email :{email} </p>
+    </div>
+  );
+}
+
+export default Post;
